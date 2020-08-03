c4a2e0c38db2ad70ea64b3305b69c0cb1e8a1763
svalenzuela <59404624+svalenzuela1@users.noreply.github.com>
2020-08-03 19:23:32 -0400

    foreach added to seed.js

--- seed.js
+++ seed.js
@@ -1,11 +1,30 @@
+require('dotenv').config()
+
 //mongoose library brought in
 const mongoose = require('mongoose')
 
-const {productModel, cartModel} = require('../models/schemas.js')
+const {productModel, cartModel} = require('./models/schemas.js')
+
+//the concatenation adds "" to the mongo database
+const mongoURI = process.env.mongoURI + "project2data"
+
+
+
+//mongoose connection 
+const db = mongoose.connection
+const mongoConfig = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+//mongo connection to mongoose
+mongoose.connect(mongoURI, mongoConfig, () => {
+    console.log('MONGO IS CONNECTED')
+})
 
 //seed data
 
-let product = [
+let products = [
     {
         name: "Final Fantasy VII Remake",
         description: "Your Favorite 90s Game In 2020 Graphics!",
@@ -13,17 +32,23 @@ let product = [
         price: 79.99
     },
     {
-        name: "Yakuza",
+        name: "Yakuza3",
         description: "Majima",
         imageURL: "https://media.giphy.com/media/UsBXUJ7egKUmTJrYca/giphy.gif",
         price: 49.99
     }
 ]
 
-product, (error, response) => {
+products, (error, response) => {
     if(error){
         console.log(error)
     }else{
         console.log(response)
     }
 }
+
+products.forEach(product => {
+   
+    productModel.create(product)
+    
+})
