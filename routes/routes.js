a7558578e479059a943c85950983f88f8548eeea
svalenzuela <59404624+svalenzuela1@users.noreply.github.com>
2020-08-03 13:05:19 -0400

    postman allows GET requests but not POST requests

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/', index)
 
-router.get('/', create)
+router.post('/', create)
+
+router.put('/:id', update)
+
+router.delete('/:id', destroy)
 
 module.exports = router;
 
